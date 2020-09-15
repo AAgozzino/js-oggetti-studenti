664e8384aab243e05cafa879918f8e008760ad84
Anna Agozzino <anna.agozzino91@gmail.com>
2020-09-15 15:11:36 +0200

    Array studenti

--- js/main.js
+++ js/main.js
@@ -4,9 +4,35 @@ $(document).ready(function(){
     "nome" : "Anna",
     "cognome" : "Agozzino",
     "eta" : 29
-  }
+  };
 
   for (var k in studente) {
     console.log(k + ": " + studente[k]);
-  }
+  };
+
+  // Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+  var classe = [
+    {
+      "nome" : "Samuele",
+      "cognome" : "Madrigali"
+    },
+    {
+      "nome" : "Elisabetta",
+      "cognome" : "Dahò"
+    },
+    {
+      "nome" : "Enrico",
+      "cognome" : "Gennari"
+    },
+    {
+      "nome" : "Laura",
+      "cognome" : "Colotto"
+    },
+  ];
+
+  for (var i = 0; i < classe.length; i++) {
+    for (var k in classe[i]) {
+      console.log(classe[i]["nome"] + " " + classe[i]["cognome"]);
+    }
+  };
 });
